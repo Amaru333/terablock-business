1edf96be1d66f29ba3327d4fada970cafefd5530
Amaru333 <amrutesharun0599@gmail.com>
2022-08-09 14:24:23 +0530

    Added slider

--- src/layout/HomepageLayout/Components/InvestmentGraph.jsx
+++ src/layout/HomepageLayout/Components/InvestmentGraph.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import UIGraph from "../../../widgets/UIGraph/UIGraph";
 import InvestmentGraphStyle from "../Styles/InvestmentGraph.module.css";
 
 function InvestmentGraph() {
@@ -10,32 +11,41 @@ function InvestmentGraph() {
             Investing is the key to building wealth<span className="text-primaryViolet">.</span>
           </b>
         </h3>
-        <p className="text-center text-white mt-4">Slide the coin and see how your crypto investment could have grown over the last five years</p>
-        <div className={["m-auto border border-1 rounded border-white px-4 py-2", InvestmentGraphStyle.half_container].join(" ")} style={{ "--bs-border-opacity": 0.2 }}>
-          <p className="text-white">Monthly Deposit</p>
+        <p className="text-center text-white mt-4 px-2">Slide the coin and see how your crypto investment could have grown over the last five years</p>
+        <div className={["m-auto border border-1 rounded border-white px-4 py-2 my-5", InvestmentGraphStyle.half_container].join(" ")} style={{ "--bs-border-opacity": 0.2 }}>
+          <p className="text-white mt-2">Monthly Deposit</p>
+          <div className={InvestmentGraphStyle.slide_container}>
+            <input type="range" min="1" max="200" className={InvestmentGraphStyle.slider} />
+          </div>
+          <div className="d-flex flex-row justify-content-between mt-2">
+            <p className="text-white">$0</p>
+            <p className="text-white">$200</p>
+          </div>
         </div>
         <div className={["row m-auto mt-3", InvestmentGraphStyle.half_container].join(" ")}>
           <div className={["col bg-white me-3 px-2 py-3 rounded", InvestmentGraphStyle.flex].join(" ")}>
             <div>
               <div className={InvestmentGraphStyle.circle}></div>
             </div>
-            <div className="ms-2">
+            <div className="ms-2 d-flex flex-column justify-content-between">
               <p className="mb-0 fs-6 text-investmentGraphBg">Savings account</p>
               <p className="mb-0 fs-5">$12,000</p>
             </div>
           </div>
           <div className={["col bg-white px-2 py-3 rounded", InvestmentGraphStyle.flex].join(" ")}>
             <div>
-              <div className={InvestmentGraphStyle.circle}></div>
+              <div className={InvestmentGraphStyle.circle} style={{ backgroundColor: "rgba(37, 74, 199, 0.6)" }}></div>
             </div>
-            <div className="ms-2">
+            <div className="ms-2 d-flex flex-column justify-content-between">
               <p className="mb-0 fs-6 text-investmentGraphBg">Crypto Investing account after 5 years</p>
               <p className="mb-0 fs-5">$58,000</p>
             </div>
           </div>
         </div>
       </div>
-      <div>GRAPH</div>
+      <div className={["p-2 p-md-5", InvestmentGraphStyle.graph_background].join(" ")}>
+        <UIGraph />
+      </div>
       <div>
         <p className={["text-cardDescriptionColor m-auto text-center", InvestmentGraphStyle.half_container].join(" ")}>
           Chart is the comparison between the past five year performance of Bitcoin (BTC) and Ethereum (ETH) and savings account with an interest rate of 0.07% yearly. Investing in cryptocurrencies involves risk of loss and performance is not guaranteed.
---
+++ src/widgets/UIGraph/UIGraph.jsx
@@ -0,0 +1,322 @@
+import React from "react";
+import { getScreenSize } from "../../functions/getScreenSize";
+import UIGraphStyle from "./UIGraph.module.css";
+
+function UIGraph() {
+  const graph_data = [
+    {
+      year: "Year 5",
+      price: [
+        {
+          five_year: 120,
+          saving: 30,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 130,
+          saving: 60,
+        },
+        {
+          five_year: 220,
+          saving: 80,
+        },
+        {
+          five_year: 190,
+          saving: 50,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 170,
+          saving: 50,
+        },
+        {
+          five_year: 100,
+          saving: 20,
+        },
+        {
+          five_year: 220,
+          saving: 70,
+        },
+        {
+          five_year: 230,
+          saving: 60,
+        },
+      ],
+    },
+    {
+      year: "2018",
+      price: [
+        {
+          five_year: 220,
+          saving: 50,
+        },
+        {
+          five_year: 210,
+          saving: 60,
+        },
+        {
+          five_year: 190,
+          saving: 50,
+        },
+        {
+          five_year: 220,
+          saving: 80,
+        },
+        {
+          five_year: 190,
+          saving: 70,
+        },
+        {
+          five_year: 150,
+          saving: 40,
+        },
+        {
+          five_year: 170,
+          saving: 50,
+        },
+        {
+          five_year: 110,
+          saving: 20,
+        },
+        {
+          five_year: 90,
+          saving: 15,
+        },
+        {
+          five_year: 80,
+          saving: 10,
+        },
+      ],
+    },
+    {
+      year: "2019",
+      price: [
+        {
+          five_year: 120,
+          saving: 30,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 130,
+          saving: 60,
+        },
+        {
+          five_year: 220,
+          saving: 80,
+        },
+        {
+          five_year: 190,
+          saving: 50,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 170,
+          saving: 50,
+        },
+        {
+          five_year: 100,
+          saving: 20,
+        },
+        {
+          five_year: 220,
+          saving: 70,
+        },
+        {
+          five_year: 230,
+          saving: 60,
+        },
+      ],
+    },
+    {
+      year: "2020",
+      price: [
+        {
+          five_year: 120,
+          saving: 30,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 130,
+          saving: 60,
+        },
+        {
+          five_year: 220,
+          saving: 80,
+        },
+        {
+          five_year: 190,
+          saving: 50,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 170,
+          saving: 50,
+        },
+        {
+          five_year: 100,
+          saving: 20,
+        },
+        {
+          five_year: 220,
+          saving: 70,
+        },
+        {
+          five_year: 230,
+          saving: 60,
+        },
+      ],
+    },
+    {
+      year: "2021",
+      price: [
+        {
+          five_year: 120,
+          saving: 30,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 130,
+          saving: 60,
+        },
+        {
+          five_year: 220,
+          saving: 80,
+        },
+        {
+          five_year: 190,
+          saving: 50,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 170,
+          saving: 50,
+        },
+        {
+          five_year: 100,
+          saving: 20,
+        },
+        {
+          five_year: 220,
+          saving: 70,
+        },
+        {
+          five_year: 230,
+          saving: 60,
+        },
+      ],
+    },
+    {
+      year: "Today",
+      price: [
+        {
+          five_year: 120,
+          saving: 30,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 130,
+          saving: 60,
+        },
+        {
+          five_year: 220,
+          saving: 80,
+        },
+        {
+          five_year: 190,
+          saving: 50,
+        },
+        {
+          five_year: 140,
+          saving: 20,
+        },
+        {
+          five_year: 170,
+          saving: 50,
+        },
+        {
+          five_year: 100,
+          saving: 20,
+        },
+        {
+          five_year: 220,
+          saving: 70,
+        },
+        {
+          five_year: 230,
+          saving: 60,
+        },
+      ],
+    },
+  ];
+  const width = getScreenSize().width;
+  return (
+    <div className="d-flex bg-white rounded px-4 px-md-5 py-4 py-md-5 justify-content-center shadow" style={{ width: "fit-content", margin: "auto" }}>
+      <div className="d-flex flex-column">
+        <div className="d-flex align-items-end">
+          {graph_data.map((each_data) => (
+            <>
+              {each_data.price.map((price, index) => (
+                <>
+                  {width > 1200 && (
+                    <div className={UIGraphStyle.five_years} style={{ height: `${price.five_year}px` }}>
+                      <div className={UIGraphStyle.savings} style={{ height: `${price.saving}px` }}></div>
+                    </div>
+                  )}
+                  {width < 1201 && width > 900 && index % 3 != 1 && (
+                    <div className={UIGraphStyle.five_years} style={{ height: `${price.five_year}px` }}>
+                      <div className={UIGraphStyle.savings} style={{ height: `${price.saving}px` }}></div>
+                    </div>
+                  )}
+                  {width < 901 && width > 768 && index % 2 != 1 && (
+                    <div className={UIGraphStyle.five_years} style={{ height: `${price.five_year}px` }}>
+                      <div className={UIGraphStyle.savings} style={{ height: `${price.saving}px` }}></div>
+                    </div>
+                  )}
+                  {width < 769 && index % 3 == 1 && (
+                    <div className={UIGraphStyle.five_years} style={{ height: `${price.five_year}px` }}>
+                      <div className={UIGraphStyle.savings} style={{ height: `${price.saving}px` }}></div>
+                    </div>
+                  )}
+                </>
+              ))}
+            </>
+          ))}
+        </div>
+        <div className="d-flex justify-content-between mt-3">
+          {graph_data.map((each_data) => (
+            <p className="mb-0 fw-bold text-primaryDark">{each_data.year}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UIGraph;
