186384c7d2ffe6d937b981499358e27f23e2fe04
Amaru333 <amrutesharun0599@gmail.com>
2022-08-17 11:02:20 +0530

    Added Coinslist header section

---
+++ pages/coinslists/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CoinsListsLayout from "../../src/layout/CoinsListsLayout/CoinsListsLayout";
+
+function index() {
+  return <CoinsListsLayout />;
+}
+
+export default index;
---
+++ src/layout/CoinsListsLayout/CoinsListsLayout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CoinsListsHeader from "./Components/CoinsListsHeader";
+
+function CoinsListsLayout() {
+  return <CoinsListsHeader />;
+}
+
+export default CoinsListsLayout;
