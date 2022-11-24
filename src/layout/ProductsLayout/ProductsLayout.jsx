206b6691520490ca1d94e6eccf793058c27788f7
Amaru333 <amrutesharun0599@gmail.com>
2022-11-24 19:25:59 +0530

    Product page

--- src/layout/ProductsLayout/ProductsLayout.jsx
+++ src/layout/ProductsLayout/ProductsLayout.jsx
@@ -4,15 +4,17 @@ import FeaturedInSection from "./Components/FeaturedInSection";
 import ProductsHeaderSection from "./Components/ProductsHeaderSection";
 import ProductsSection from "./Components/ProductsSection";
 import RecentNewsSection from "./Components/RecentNewsSection";
+import StartJourney from "./Components/StartJourney";
 
 function ProductsLayout() {
   return (
     <div>
       <ProductsHeaderSection />
       <ProductsSection />
-      <DescriptionSection />
-      <RecentNewsSection />
+      {/* <DescriptionSection /> */}
+      {/* <RecentNewsSection /> */}
       <FeaturedInSection />
+      <StartJourney />
     </div>
   );
 }
