cdd36452341e18d7fa7fc173190e44505e529035
Niladri Pal <niladri.pal@nettantra.net>
2023-01-04 12:34:46 +0530

    Fixed conflict and code cleanup

--- src/common/Cards/HorizontalCards/HorizontalCards.jsx
+++ src/common/Cards/HorizontalCards/HorizontalCards.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 import HorizontalCardsStyle from "./HorizontalCards.module.css";
 
-function HorizontalCards({ data, description_1, description_2, title}) {
+function HorizontalCards({ data, description_1, description_2, title }) {
   const DifficultyTag = ({ level }) => {
     const color = {
       Beginner: "#00C077",
@@ -20,25 +20,25 @@ function HorizontalCards({ data, description_1, description_2, title}) {
   const NewsCard = ({ image, title, description, difficulty, time, slug }) => {
     return (
       <Link href={`learn/${slug}`}>
-      <div className="card position-relative" style={{ width: "18rem", zIndex: 2, borderRadius: "16px", padding: "12px", boxShadow: "2px 2px 24px rgba(0, 0, 0, 0.06)", minHeight: '420px', cursor: 'pointer' }}>
-        <img src={image} className="card-img" />
-        <div className="pt-3 text-truncate-container">
-          <h5 className="card-title" style={{ fontWeight: "600" }}>
-            {title}
-          </h5>
-          <div className="d-flex justify-content-between mb-2" style={{ fontSize: "14px" }}>
-            <DifficultyTag level={difficulty} />
-            <div className="d-flex align-items-center">
-              <img src="/assets/icons/time.svg" />
-              <span style={{ color: "#797979", marginLeft: "5px", fontWeight: "600" }}>{time} mins</span>
+        <div className="card position-relative" style={{ width: "18rem", zIndex: 2, borderRadius: "16px", padding: "12px", boxShadow: "2px 2px 24px rgba(0, 0, 0, 0.06)", minHeight: '420px', cursor: 'pointer' }}>
+          <img src={image} className="card-img" />
+          <div className="pt-3 text-truncate-container">
+            <h5 className="card-title" style={{ fontWeight: "600" }}>
+              {title}
+            </h5>
+            <div className="d-flex justify-content-between mb-2" style={{ fontSize: "14px" }}>
+              <DifficultyTag level={difficulty} />
+              <div className="d-flex align-items-center">
+                <img src="/assets/icons/time.svg" />
+                <span style={{ color: "#797979", marginLeft: "5px", fontWeight: "600" }}>{time} mins</span>
+              </div>
             </div>
-          </div>
-          <p className={"card-text mt-3 truncate"} style={{ color: "#707070", fontSize: "14px", width: '250px' }}>
-            {description}
-          </p>
+            <p className={"card-text mt-3 truncate"} style={{ color: "#707070", fontSize: "14px", width: '250px' }}>
+              {description}
+            </p>
             <span style={{ color: "#0052FF", fontWeight: "700" }} className='position-absolute bottom-0 pb-2'>Read More →</span>
+          </div>
         </div>
-      </div>
       </Link>
     );
   };
