4a7605ff7d840b5bb4142a1e281b30bb52a399c4
Niladri Pal <niladri.pal@nettantra.net>
2023-01-05 18:28:30 +0530

    added navigation for popular posts and added tags

--- src/layout/LearnLayout/Components/LearnHeroComponent.jsx
+++ src/layout/LearnLayout/Components/LearnHeroComponent.jsx
@@ -9,7 +9,8 @@ function LearnHeroComponent() {
       <div className="py-3" style={{ maxWidth: "1700px", margin: "auto", width: width > 1499 ? "70%" : width > 940 ? "81%" : "100%" }}>
         <div className="row mx-0" style={{ zIndex: 1 }}>
           <div className="mb-5 mt-0 mt-md-5 px-4 px-md-4 col-12 col-md-7 pe-0 pe-md-5 d-flex flex-column justify-content-center text-center pe-4 text-md-start">
-            <div style={{ width: width > 768 ? "80%" : "100%" }}>
+            {/* <div style={{ width: width > 768 ? "100%" : "100%" }}> */}
+            <div style={{ width: "100%" }}>
               <h1 className="fw-bold" style={{ color: "#1b2b6b", fontSize: width > 767 ? "40px" : "23px" }}>
                 Everything you need to know, about Crypto & Blockchain
               </h1>
