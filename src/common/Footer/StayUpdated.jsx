35551ae895d79e0ad0d082957b9001a8c8a968b3
Soneshwar <soneshwarsinghblog@gmail.com>
2023-02-25 17:56:17 +0530

    minor fixes

--- src/common/Footer/StayUpdated.jsx
+++ src/common/Footer/StayUpdated.jsx
@@ -14,9 +14,20 @@ function StayUpdated() {
         <div>
           <UIButton type="primary">Sign Up</UIButton>
         </div> */}
+        <div dangerouslySetInnerHTML={{ __html:
+            "<script>"+
+            "(function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){"+
+            "var c={ a:arguments,q:[]};var r=this.push(c);return \"number\"!=typeof r?r:f.bind(c.q);}"+
+            "f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);"+
+            "var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));"+
+            "_.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');"+
+            "var ml_account = ml('accounts', '3712445', 'n4a2k0o1k0', 'load');"+
+            "</script>"
+        }} />
         <div class="ml-form-embed"
           data-account="3712445:n4a2k0o1k0"
-          data-form="5253668:n7a1k9">
+          data-form="5253668:n7a1k9"
+          style={{ minWidth: width > 767 ? "400px" : "300px" }}>
         </div>
       </div>
     </div>
