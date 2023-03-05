de233757587cf752d877724363210d33c6299b88
Soneshwar <soneshwarsinghblog@gmail.com>
2023-03-06 01:50:59 +0530

    empowers spelling

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -60,9 +60,9 @@ function Header() {
             <p className="mb-0 me-3 pb-1" style={{ fontSize: "15px", fontWeight: 500, cursor: "pointer", borderBottom: "2px solid #E3297E" }}>
               <a href="https://app.terablock.com/login" target='_blank' style={{ "text-decoration": "none", "color": "#111111" }}> Launch App → </a>
             </p>
-            {/* <button className={["bg-primaryBlue text-white px-4 py-2 mx-2", HeaderStyle.button, HeaderStyle.button_primary].join(" ")} onClick={() => openNewTab('https://app.terablock.com/login')} onMouseOut={() => setButtonText("Get Started")}>
+            <button className={["bg-primaryBlue text-white px-4 py-2 mx-2", HeaderStyle.button, HeaderStyle.button_primary].join(" ")} onClick={() => openNewTab('https://app.terablock.com/login')} onMouseOut={() => setButtonText("Get Started")}>
               {buttonText}
-            </button> */}
+            </button>
           </div>
         </div>
       </div>
